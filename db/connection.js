531af9165e8d8da35ed9c70ed71ddb5df41e42a2
brittany spratford <britsprat@gmail.com>
2022-05-08 22:57:03 -0700

    further updates for lib folder

--- db/connection.js
+++ db/connection.js
@@ -3,11 +3,15 @@ const db = require('mysql2-promise')();
 
 db.configure({
   host: 'localhost',
-  // Your MySQL username,
+
+  // MySQL username,
   user: 'root',
-  // Your MySQL password
+
+  // MySQL password
   password: 'Bas2022@!',
-  database: 'cms_employee'
+
+  // enter database
+  database: 'em-tracker'
 });
 
 module.exports = db;
---
+++ db/db.sql
@@ -0,0 +1,4 @@
+
+DROP DATABASE IF EXISTS em-tracker;
+CREATE DATABASE em-tracker;
+USE em-tracker;
